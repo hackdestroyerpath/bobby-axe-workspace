86cfeba50e980c13db7bf68e4d3d00d866c0db23
hackdestroyerpath <wisokyjaden@gmail.com>
2026-03-24 04:05:43 +0300

    Add Maffi output formatter and contract coverage

--- maffi/__init__.py
+++ maffi/__init__.py
@@ -1,6 +1,17 @@
 from .decision_engine import decide
+from .formatter import maffi_output_to_dict, maffi_output_to_json, normalize_decision_trace, normalize_rationale
 from .payload_builder import build_maffi_payload, payload_to_dict
 from .replay import deterministic_replay
 from .validator import validate_payload
 
-__all__ = ["decide", "deterministic_replay", "validate_payload", "build_maffi_payload", "payload_to_dict"]
+__all__ = [
+    "decide",
+    "deterministic_replay",
+    "validate_payload",
+    "build_maffi_payload",
+    "payload_to_dict",
+    "maffi_output_to_dict",
+    "maffi_output_to_json",
+    "normalize_rationale",
+    "normalize_decision_trace",
+]
